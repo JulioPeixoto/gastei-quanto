fd6cbd5c11cdda3fda54d5e05264b3567e150a9a
Julio Rafael <juliorafaelnft@hotmail.com>
2025-12-02 00:56:19 -0300

    f

--- src/internal/parser/model.go
+++ src/internal/parser/model.go
@@ -3,11 +3,11 @@ package parser
 import "time"
 
 type Transaction struct {
-	Date	     time.Time `json:"date"`
-	Description  string    `json:"description"`
-	Category     string    `json:"category"`
-	Amount       float64   `json:"amount"`
-} 
+	Date        time.Time `json:"date"`
+	Description string    `json:"description"`
+	Category    string    `json:"category"`
+	Amount      float64   `json:"amount"`
+}
 
 type UploadResponse struct {
 	Message      string        `json:"message"`
